9bdf784b1674fb028a2bea51751794215dd8d63a
Younes202 <sghyeryounes@gmail.com>
2023-09-21 21:51:11 +0100

    set Repo Empl

---
+++ src/main/java/com/younes/caisse/repositories/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.younes.caisse.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.younes.caisse.Entities.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // You can add custom query methods here if needed
+}
